dc897c5f322f419a91394dc77265617cf1d0f379
Ahmed Ali Ali <81569432+AhmedAliAli811@users.noreply.github.com>
2024-08-27 23:35:33 +0300

    Time: 4965 ms (7.34%) | Memory: 251.8 MB (5.09%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.py
+++ 45-jump-game-ii/jump-game-ii.py
@@ -1,11 +1,32 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        ind , cur , cnt = 0 , 0 , 0
-        while ind < len(nums) - 1 :
-            maxmm = 0
-            for j in range(cur , ind + 1):
-                maxmm = max(maxmm, j + nums[j])
-            cur = ind + 1
-            ind = maxmm
-            cnt += 1
-        return cnt
+        if len(nums) == 1:
+            return 0
+        n = len(nums)
+        adj = [[] for _ in range(n)]
+
+        vis = [False] * len(nums)
+        dis = [0] * len(nums)
+
+        for i in range(len(nums)):
+            for j in range(nums[i]+1):
+                if i + j <= len(nums) - 1:
+                    adj[i].append(j+i)
+                    adj[j+i].append(i)
+
+
+        def bfs (node):
+            q = deque()
+
+            q.append(node)
+            vis[node] = True
+            dis[node] = 0
+            while len(q) > 0:
+                node = q.popleft()
+                for child in adj[node]:
+                    if vis[child] == False:
+                        q.append(child)
+                        vis[child] = True
+                        dis[child] = dis[node] + 1
+        bfs(0)
+        return dis[len(nums) - 1 ]
