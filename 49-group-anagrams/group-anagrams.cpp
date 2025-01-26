10279cecba54543d18f05912497c283325c14088
Ahmed Ali Ali <81569432+AhmedAliAli811@users.noreply.github.com>
2025-01-26 16:31:28 +0200

    Time: 16 ms (65.03%) | Memory: 25.2 MB (64.45%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -2,18 +2,14 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>> res;
-        vector<string> temp = strs;
-        map<string,vector<int>> m;
+        map<string,vector<string>> m;
         for (int i = 0 ; i<strs.size(); i++) {
-            sort(strs[i].begin(), strs[i].end());
-            m[strs[i]].push_back(i);
+            string temp = strs[i];
+            sort(temp.begin(), temp.end());
+            m[temp].push_back(strs[i]);
         }
         for (auto it=m.begin(); it!=m.end(); it++) {
-            vector<string> temp2;
-            for (auto num : it->second) {
-                temp2.push_back(temp[num]);
-            }
-            res.push_back(temp2);
+            res.push_back(it->second);
         }
         return res;
     }
